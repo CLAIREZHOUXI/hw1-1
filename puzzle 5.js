d283f88fe543e2171e937fa9366c80bc954b107c
CLAIREZHOUXI <35748942+CLAIREZHOUXI@users.noreply.github.com>
2018-01-29 09:11:19 -0800

    Update puzzle 5.js

--- puzzle 5.js
+++ puzzle 5.js
@@ -16,3 +16,25 @@ if(var B==blue){down};
 right();
 right();
 right();
+
+// right();
+// right();
+// function move(){
+//   getColor();
+// var A = getColor()
+// if(getColor() == "red" ){
+//   up();}
+// if(getColor() == "blue" ){
+//   down();}
+// }
+
+// move();
+// right();
+// right();
+// move();
+// right();
+// right();
+// move();
+// right();
+// right();
+// right();
